a36ea8e343bf5c426ba9bde435a1f9125c90ea66
Dasha Goulyaeva <dashagoulyaeva@gmail.com>
2026-02-01 15:32:46 +0300

    fixing some crap

--- test_sd.py
+++ test_sd.py
@@ -1,17 +1,18 @@
 from diffusers import StableDiffusionPipeline
 import torch
 
-# Загружаем модель
+# Загружаем модель 
+pipe = StableDiffusionPipeline.from_pretrained(
     "runwayml/stable-diffusion-v1-5"
 ).to("cpu")  # Генерация на CPU
 
-# Промпт для генерации, заменить на промт из файла
+# Промпт, потом поменять на промпт из файла
 prompt = "абстрактная весна, акварель, светлая атмосфера"
 
 # Генерируем картинку
 image = pipe(prompt).images[0]
 
-# Сохраняем результат, потом заменить на генерацию по системному времени вида sun0102 (воскресенье первое февраля)
+# Сохраняем результат. потом поменять на сохранение типа sun01022026
 image.save("test_image.png")
 
 print("Генерация завершена! Файл: test_image.png")
